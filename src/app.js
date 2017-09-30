38427eec1eb756a80681bbe4ce409fb5bdd60875
Lincerossa90 <marcello.luatti@alkemy.com>
2017-09-30 20:29:54 +0200

    first psuh

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Map from './components'
+
+ReactDOM.render(<Map/>, document.getElementById('ss'));
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import style from './Map.scss'
+
+const Map = () => {
+	return(
+		<div className={style.test}>siiiiii</div>
+	)}
+
+export default Map
